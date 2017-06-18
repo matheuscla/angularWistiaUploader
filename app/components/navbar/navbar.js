3d666c8cb6a5fbf26659d1887796ff8e583bc87c
matheuscla <matheuscla@yahoo.com.br>
2017-06-18 20:19:44 -0300

    Navbar component

---
+++ app/components/navbar/navbar.js
@@ -0,0 +1,5 @@
+angular
+  .module('angularWistiaUpoloaderApp')
+  .component('navbar', {
+    templateUrl: './components/navbar/navbar.html'
+  })
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,27 +9,4 @@
  * Main module of the application.
  */
 angular
-  .module('angularWistiaUpoloaderApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+  .module('angularWistiaUpoloaderApp', [])
