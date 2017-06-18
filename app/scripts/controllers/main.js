4500697658c32d34794cb5eafc6a43ab73bd7eaa
matheuscla <matheuscla@yahoo.com.br>
2017-06-18 19:35:58 -0300

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularWistiaUpoloaderApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularWistiaUpoloaderApp
+ */
+angular.module('angularWistiaUpoloaderApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
